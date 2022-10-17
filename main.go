4ed54836a76501c965bb6dc23fe0919ea521506c
anthdm <cryptoanthdm@gmail.com>
2022-10-17 11:39:40 +0200

    WIP market making basics

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/anthdm/crypto-exchange/client"
+	"github.com/anthdm/crypto-exchange/mm"
 	"github.com/anthdm/crypto-exchange/server"
 )
 
-const ethPrice = 1281.0
-
 func main() {
 	go server.StartServer()
-
 	time.Sleep(1 * time.Second)
 
 	c := client.NewClient()
 
-	go makeMarketSimple(c)
-	time.Sleep(2 * time.Second)
+	cfg := mm.Config{
+		UserID:         8,
+		OrderSize:      10,
+		MinSpread:      100,
+		MakeInterval:   1 * time.Second,
+		SeedOffset:     400,
+		ExchangeClient: c,
+	}
+	maker := mm.NewMakerMaker(cfg)
 
+	maker.Start()
+
+	time.Sleep(2 * time.Second)
 	go marketOrderPlacer(c)
 
 	select {}
@@ -49,59 +56,6 @@ func marketOrderPlacer(c *client.Client) {
 		if err != nil {
 			panic(err)
 		}
-
-		<-ticker.C
-	}
-}
-
-func seedMarket(c *client.Client) {
-	currentPrice := ethPrice // asyns call to fetch the price
-	priceOffset := 100.0
-
-	bidOrder := client.PlaceOrderParams{
-		UserID: 8,
-		Bid:    true,
-		Price:  currentPrice - priceOffset,
-		Size:   10,
-	}
-
-	_, err := c.PlaceLimitOrder(&bidOrder)
-	if err != nil {
-		panic(err)
-	}
-
-	askOrder := client.PlaceOrderParams{
-		UserID: 8,
-		Bid:    false,
-		Price:  currentPrice + priceOffset,
-		Size:   10,
-	}
-
-	_, err = c.PlaceLimitOrder(&askOrder)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func makeMarketSimple(c *client.Client) {
-	ticker := time.NewTicker(1 * time.Second)
-	for {
-		bestAsk, err := c.GetBestAsk()
-		if err != nil {
-			panic(err)
-		}
-		bestBid, err := c.GetBestBid()
-		if err != nil {
-			panic(err)
-		}
-
-		if bestAsk == 0 && bestBid == 0 {
-			seedMarket(c)
-			continue
-		}
-
-		fmt.Println("best Ask", bestAsk)
-		fmt.Println("best bid", bestBid)
 
 		<-ticker.C
 	}
---
+++ mm/maker.go
@@ -0,0 +1,113 @@
+package mm
+
+import (
+	"time"
+
+	"github.com/anthdm/crypto-exchange/client"
+	"github.com/sirupsen/logrus"
+)
+
+type Config struct {
+	UserID         int64
+	OrderSize      float64
+	MinSpread      float64
+	SeedOffset     float64
+	ExchangeClient *client.Client
+	MakeInterval   time.Duration
+}
+
+type MarketMaker struct {
+	userID         int64
+	orderSize      float64
+	minSpread      float64
+	seedOffset     float64
+	exchangeClient *client.Client
+	makeInterval   time.Duration
+}
+
+func NewMakerMaker(cfg Config) *MarketMaker {
+	return &MarketMaker{
+		userID:         cfg.UserID,
+		orderSize:      cfg.OrderSize,
+		minSpread:      cfg.MinSpread,
+		seedOffset:     cfg.SeedOffset,
+		exchangeClient: cfg.ExchangeClient,
+		makeInterval:   cfg.MakeInterval,
+	}
+}
+
+func (mm *MarketMaker) Start() {
+	logrus.WithFields(logrus.Fields{
+		"id":           mm.userID,
+		"orderSize":    mm.orderSize,
+		"makeInterval": mm.makeInterval,
+		"minSpread":    mm.minSpread,
+	}).Info("starting market maker")
+
+	go mm.makerLoop()
+}
+
+func (mm *MarketMaker) makerLoop() {
+	ticker := time.NewTicker(mm.makeInterval)
+
+	for {
+		bestBid, err := mm.exchangeClient.GetBestBid()
+		if err != nil {
+			logrus.Error(err)
+			break
+		}
+
+		bestAsk, err := mm.exchangeClient.GetBestAsk()
+		if err != nil {
+			logrus.Error(err)
+			break
+		}
+
+		if bestAsk == 0 && bestBid == 0 {
+			if err := mm.seedMarket(); err != nil {
+				logrus.Error(err)
+				break
+			}
+		}
+
+		<-ticker.C
+	}
+}
+
+func (mm *MarketMaker) seedMarket() error {
+	currentPrice := simulateFetchCurrentETHPrice()
+
+	logrus.WithFields(logrus.Fields{
+		"currentETHPrice": currentPrice,
+		"seedOffset":      mm.seedOffset,
+	}).Info("orderbooks empty => seeding market!")
+
+	bidOrder := &client.PlaceOrderParams{
+		UserID: mm.userID,
+		Size:   mm.orderSize,
+		Bid:    true,
+		Price:  currentPrice - mm.seedOffset,
+	}
+	_, err := mm.exchangeClient.PlaceLimitOrder(bidOrder)
+	if err != nil {
+		return err
+	}
+
+	askOrder := &client.PlaceOrderParams{
+		UserID: mm.userID,
+		Size:   mm.orderSize,
+		Bid:    false,
+		Price:  currentPrice + mm.seedOffset,
+	}
+	_, err = mm.exchangeClient.PlaceLimitOrder(askOrder)
+
+	return err
+}
+
+// this will simulate a call to an other exchange fetching
+// the current ETH price so we can offset both for a bid and ask.
+func simulateFetchCurrentETHPrice() float64 {
+	time.Sleep(80 * time.Millisecond)
+
+	return 1000.0
+}
